45554fe1d4865da7c22bdc19f16de4bb6c86f68c
Mavine1 <mavinemalin@gmail.com>
2024-07-03 11:56:00 +0300

    vessels left still kitchen widely

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,9 +12,9 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                
+                David 
               </span>
-              Blog
+              Inspiration
             </Link>
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
@@ -39,7 +39,6 @@ export default function FooterCom() {
             </div>
             <div>
               <Footer.Title title='Follow us' />
-              <Footer.LinkGroup col>
                 <Footer.Link
                   href='https://www.github.com/sahandghavidel'
                   target='_blank'
