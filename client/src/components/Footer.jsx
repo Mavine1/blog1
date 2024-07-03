d9dbcd9a3a66a5c478986690a6722fa2c5220c61
Mavine1 <mavinemalin@gmail.com>
2024-07-03 12:02:01 +0300

    slept row lay directly major

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -31,24 +31,6 @@ export default function FooterCom() {
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title='Follow us' />
-              <Footer.LinkGroup col>
-                <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Github
-                </Footer.Link>
-                <Footer.Link href='#'>Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Legal' />
-              <Footer.LinkGroup col>
-                <Footer.Link href='#'>Privacy Policy</Footer.Link>
-                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
             </div>
           </div>
         </div>
