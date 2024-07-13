be639207c7b32cc402cb1ac8df9f0963c29582f8
Mavine1 <mavinemalin@gmail.com>
2024-07-13 21:20:01 +0300

    fierce exclaimed pole hall pour

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Modal, TextInput } from 'flowbite-react';
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
@@ -47,7 +47,7 @@ export default function DashProfile() {
     if (imageFile) {
       uploadImage();
     }
-  }, [imageFile, uploadImage]);
+  }, [imageFile]);
 
   const uploadImage = async () => {
     // service firebase.storage {
