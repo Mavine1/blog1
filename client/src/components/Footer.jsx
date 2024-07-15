a60035f89c923cceceb31c6d02f63fb3f1be757d
Mavine1 <mavinemalin@gmail.com>
2024-07-15 14:16:01 +0300

    date vertical someone month excitement

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import { Footer } from 'flowbite-react';
-import { Link } from 'react-router-dom';
 import { BsFacebook, BsInstagram, BsTwitter,} from 'react-icons/bs';
 export default function FooterCom() {
   return (
