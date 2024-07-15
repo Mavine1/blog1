38a120b698d4a434214bcfa689e01fc9700c34a3
Mavine1 <mavinemalin@gmail.com>
2024-07-15 12:40:01 +0300

    supply gentle wrapped upper closely

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,8 +15,8 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl lg:text-6xl'>Welcome to my Blog</h1>
+      <div className='flex flex-col gap-4 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl lg:text-6xl'>Ignite Your Faith, Illuminate Your Path</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
         </p>
       </div>
