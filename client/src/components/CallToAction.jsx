bc99cf73791ce7d65c95928ec18854a916604cac
Mavine1 <mavinemalin@gmail.com>
2024-07-15 12:18:00 +0300

    substance purpose discovery brought needle

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -37,7 +37,7 @@ export default function CallToAction() {
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://www.shutterstock.com/image-vector/read-more-symbol-paint-brush-stroke-2012420402" alt="Bible study" />
+            <img src="https://images.unsplash.com/photo-1527784281695-866fa715d9d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Bible study" />
         </div>
       </div>
     </>
