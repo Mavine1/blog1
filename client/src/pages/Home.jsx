87bcbb4d0a7f938df007a38090a3db830056c8e3
Mavine1 <mavinemalin@gmail.com>
2024-07-15 12:36:00 +0300

    do diameter particularly pan income

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,11 +16,10 @@ export default function Home() {
   return (
     <div>
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <h1 className='text-3xl lg:text-6xl'>Welcome to my Blog</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
         </p>
       </div>
-
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
