10ac36a227e3af289f373752b4323be82f34dd3b
Mavine1 <mavinemalin@gmail.com>
2024-07-15 12:52:01 +0300

    folks fruit spoken fish activity

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,8 +20,8 @@ export default function Home() {
         <p className='text-gray-500 text-xs sm:text-sm'>
           {/* Add your subheading or description here */}
         </p>
-        
       </div>
+      <hr className="my-8 border-gray-300 max-w-6xl mx-auto" />
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-4'>
