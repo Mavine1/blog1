ee6c72ad953f78c6e3cef9c93bfd53e49320664e
Mavine1 <mavinemalin@gmail.com>
2024-07-15 11:14:01 +0300

    warn environment society caught check

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -105,8 +105,8 @@ export default function CreatePost() {
           >
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>Biblical Reflections</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value='reactjs'>Daily Devotionals</option>
+            <option value='nextjs'>Christian Living</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
