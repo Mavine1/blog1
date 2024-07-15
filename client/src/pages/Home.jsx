185f872c8cc14f1c52f522573ea887d5b5265e72
Mavine1 <mavinemalin@gmail.com>
2024-07-15 12:42:01 +0300

    likely made buffalo tone temperature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,14 +15,43 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className='flex flex-col gap-4 p-28 px-3 max-w-6xl mx-auto '>
+     Copy<div className='flex flex-col gap-4 p-12 px-3 max-w-6xl mx-auto'>
+  <h1 className='text-3xl lg:text-6xl'>Ignite Your Faith, Illuminate Your Path</h1>
+  <p className='text-gray-500 text-xs sm:text-sm'>
+  </p>
+</div>
+<div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
+  {posts && posts.length > 0 && (
+    <div className='flex flex-col gap-4'>
+      <div className='flex flex-wrap gap-4'>
+        {posts.map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))}
+      </div>
+    </div>
+  )}
+</div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+
+  return (
+    <div>
+      <div className='flex flex-col gap-4 p-12 px-3 max-w-6xl mx-auto'>
         <h1 className='text-3xl lg:text-6xl'>Ignite Your Faith, Illuminate Your Path</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
         </p>
       </div>
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
         {posts && posts.length > 0 && (
-          <div className='flex flex-col gap-6'>
+          <div className='flex flex-col gap-4'>
             <div className='flex flex-wrap gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
@@ -39,4 +68,4 @@ export default function Home() {
       </div>
     </div>
   );
-}
+}}
