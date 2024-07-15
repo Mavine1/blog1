d3febf5a9f05595cfe3a1f251d3cacc5b0caae94
Mavine1 <mavinemalin@gmail.com>
2024-07-15 11:40:01 +0300

    increase grandmother fall welcome handsome

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,10 +5,10 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Want to dive deeper into God's Word?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+            Explore the Bible with these online resources
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
                 <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
