2f8e6418f3ae52bef65128aeb2b18e304bd90619
Mavine1 <mavinemalin@gmail.com>
2024-07-15 11:38:00 +0300

    swimming fifteen ball monkey eventually

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Read More</h1>
-      <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <p className='text-md text-gray-500'>Dive deeper into God's Word and discover more inspiring content in our latest posts!</p>
       <CallToAction />
     </div>
   )
