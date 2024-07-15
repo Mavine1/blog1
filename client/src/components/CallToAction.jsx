6be7a1df3e88fe9a4a0bb25a50388c7a0790d4e8
Mavine1 <mavinemalin@gmail.com>
2024-07-15 11:52:00 +0300

    usually hold road may society

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -17,7 +17,7 @@ export default function CallToAction() {
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://www.freepik.com/free-photo/holy-bible-with-rays-light-coming-out-ai-generative_41369575.htm#query=bible&position=20&from_view=keyword&track=sph&uuid=85902157-ab3b-4fac-965b-6a94a997d7d2" />
         </div>
     </div>
   )
