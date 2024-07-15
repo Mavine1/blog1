4e9fa0b4c083eaaab703a57fbae427e6dd9974b0
Mavine1 <mavinemalin@gmail.com>
2024-07-15 12:46:00 +0300

    though powerful separate or program

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,40 +13,13 @@ export default function Home() {
     };
     fetchPosts();
   }, []);
-  return (
-    <div>
-     Copy<div className='flex flex-col gap-4 p-12 px-3 max-w-6xl mx-auto'>
-  <h1 className='text-3xl lg:text-6xl'>Ignite Your Faith, Illuminate Your Path</h1>
-  <p className='text-gray-500 text-xs sm:text-sm'>
-  </p>
-</div>
-<div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
-  {posts && posts.length > 0 && (
-    <div className='flex flex-col gap-4'>
-      <div className='flex flex-wrap gap-4'>
-        {posts.map((post) => (
-          <PostCard key={post._id} post={post} />
-        ))}
-      </div>
-    </div>
-  )}
-</div>
-            <Link
-              to={'/search'}
-              className='text-lg text-teal-500 hover:underline text-center'
-            >
-              View all posts
-            </Link>
-          </div>
-        )}
-          </div>
-        )
-      }    </div>
+
   return (
     <div>
       <div className='flex flex-col gap-4 p-12 px-3 max-w-6xl mx-auto'>
         <h1 className='text-3xl lg:text-6xl'>Ignite Your Faith, Illuminate Your Path</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
+          {/* Add your subheading or description here */}
         </p>
       </div>
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
@@ -67,4 +40,5 @@ export default function Home() {
         )}
       </div>
     </div>
-  )}}
+  );
+}
