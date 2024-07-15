fcf1561072c72d4d8ec6290f5c4432f70d682b97
Mavine1 <mavinemalin@gmail.com>
2024-07-15 10:58:01 +0300

    bee hit plant across real

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -43,6 +43,7 @@ export default function CreatePost() {
         (error) => {
           setImageUploadError('Image upload failed');
           setImageUploadProgress(null);
+          console.error(error);
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
