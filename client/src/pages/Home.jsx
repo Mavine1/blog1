c449245282ad899af7b623c32cfeb287168a65a6
Mavine1 <mavinemalin@gmail.com>
2024-07-20 14:36:00 +0300

    practical cage doubt kind lay

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,9 +22,10 @@ export default function Home() {
         </p>
       </div>
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
+
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-4'>
-            <div className='flex flex-wrap gap-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
