a553b6a910a72286a3fb8b5f4a5ba9a6f80dd864
Mavine1 <mavinemalin@gmail.com>
2024-07-20 15:16:00 +0300

    fifteen island rich paper sudden

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,10 +21,10 @@ export default function Home() {
            Discover daily inspiration and spiritual insights to guide your journey
         </p>
       </div>
-      <div className='max-w-6xl  px-10 flex flex-col gap-6 py-4'>
+      <div className='max-w-6xl  px-10  gap-6 py-4'>
         {posts && posts.length > 0 && (
-          <div className='flex flex-col gap-4'>
-            <div className='flex flex-wrap gap-4 sm:flex-nowrap lg:grid lg:grid-cols-3 lg:gap-8'>
+          <div className='flex flex-col gap-6'>
+            <div className='flex flex-wrap gap-4 sm:flex-nowrap  lg:gap-8'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
