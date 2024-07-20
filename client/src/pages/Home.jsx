273dc7e18ef6cdb4f4c334804f56f6c5b379a4d6
Mavine1 <mavinemalin@gmail.com>
2024-07-20 14:46:00 +0300

    does numeral remarkable hour lay

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
            Discover daily inspiration and spiritual insights to guide your journey
         </p>
       </div>
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-2 px-2 py-2'>
+      <div className='max-w-6xl mx-auto  flex flex-col gap-2 px-2 p-2'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-2'>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
