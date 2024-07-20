c79b251c68bcebf856913dade64a0266d8a6fadf
Mavine1 <mavinemalin@gmail.com>
2024-07-20 14:54:00 +0300

    iron fact smaller tax life

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,16 +15,16 @@ export default function Home() {
 
   return (
     <div>
-      <div className='flex flex-col gap-4 p-4 px-2 max-w-6xl mx-auto'>
+      <div className='flex flex-col gap-4 p-4 px-3 max-w-6xl mx-auto'>
         <h1 className='text-2xl '><em>Ignite Your Faith, Illuminate Your Path</em></h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
            Discover daily inspiration and spiritual insights to guide your journey
         </p>
       </div>
-      <div className='max-w-6xl mx-auto  flex flex-col gap-2 px-2 p-2'>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
         {posts && posts.length > 0 && (
-          <div className=' flex flex-col- gap-4'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  flex-wrap gap-4'>
+          <div className='flex flex-col gap-4'>
+            <div className='flex flex-wrap gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
