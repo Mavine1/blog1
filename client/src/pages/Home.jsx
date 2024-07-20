e406f601e1cfc19553efd0b46ba98654745229a4
Mavine1 <mavinemalin@gmail.com>
2024-07-20 15:18:01 +0300

    view slowly tightly limited nation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,13 +15,13 @@ export default function Home() {
 
   return (
     <div>
-      <div className='flex flex-col gap-4 p-4 px-10 max-w-6xl '>
+      <div className='flex flex-col gap-4 p-4 mx-auto px-3 max-w-6xl '>
         <h1 className='text-2xl '><em>Ignite Your Faith, Illuminate Your Path</em></h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
            Discover daily inspiration and spiritual insights to guide your journey
         </p>
       </div>
-      <div className='max-w-6xl  px-10  gap-6 py-4'>
+      <div className='max-w-6xl  mx-auto px-3 gap-6 py-4'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
             <div className='flex flex-wrap gap-4 sm:flex-nowrap  lg:gap-8'>
