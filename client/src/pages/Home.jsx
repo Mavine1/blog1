82aecd5c7b4a89747ca5e919238fe32cb946a983
Mavine1 <mavinemalin@gmail.com>
2024-07-20 15:02:01 +0300

    quickly younger lips forest child

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-4'>
-            <div className='flex flex-wrap gap-4'>
+            <div className='flex flex-wrap-3 gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
