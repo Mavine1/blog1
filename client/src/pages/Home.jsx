482addcd4bc820b577b4fa823d9df4d768da7c5c
Mavine1 <mavinemalin@gmail.com>
2024-07-20 14:40:00 +0300

    fine adjective sing follow short

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,8 +23,8 @@ export default function Home() {
       </div>
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-2 py-4'>
         {posts && posts.length > 0 && (
-          <div className='flex flex-col gap-2'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2'>
+          <div className='flex flex-col gap-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
