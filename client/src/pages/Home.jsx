a0f539143231a284c59fdfdb0ed4998dce9ad169
Mavine1 <mavinemalin@gmail.com>
2024-07-20 15:12:00 +0300

    build couple give sell jack

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
            Discover daily inspiration and spiritual insights to guide your journey
         </p>
       </div>
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-4'>
+      <div className='max-w-6xl  p-3 flex flex-col gap-6 py-4'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-4'>
             <div className='flex flex-wrap gap-4 sm:flex-nowrap lg:grid lg:grid-cols-3 lg:gap-8'>
