d56c77f9b82cba59bbf6ff7617c7393d8a858543
Mavine1 <mavinemalin@gmail.com>
2024-08-22 16:36:00 +0300

    kill safety plastic water captured

--- api/index.js
+++ api/index.js
@@ -8,9 +8,11 @@ import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -20,7 +22,9 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+// Handle `__dirname` in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -41,17 +45,20 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
+// Define routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+// Serve static files
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
