5ec4c44eb7e57db0c42d656b282e9215d66c7aa3
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2025-01-22 08:03:22 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://david-inspiration.netlify.app',
         secure: false,
       },
     },
