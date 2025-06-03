5d2355247100b78a21d3d8ad6b64a221cf383447
Mavine1 <mavinemalin@gmail.com>
2025-06-03 21:16:00 +0300

    silence zulu given information basis

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
-
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
